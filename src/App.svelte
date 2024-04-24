fc7202f7eb9c92512f29198845f11fd1e45d4253
Pig Fang <g-plane@hotmail.com>
2024-04-24 11:32:02 +0800

    add UI framework

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,24 @@
 <script lang="ts">
+  import { Card, StyleFromScheme } from 'm3-svelte'
+  import { dark, light } from './theme'
 </script>
+
+<StyleFromScheme lightScheme={light} darkScheme={dark} />
+
+<main>
+  <Card type="filled">
+    <img
+      src="https://raw.githubusercontent.com/samiheikki/javascript-guessing-game/master/static/logos/cyclejs.png"
+      alt=""
+    >
+  </Card>
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
